ee49238569ef9b21e6502bb3e5486ff92260b813
AbhinayaRavula <147133014+AbhinayaRavula@users.noreply.github.com>
2025-01-02 11:58:53 +0530

    Add files via upload

--- First.java
+++ First.java
@@ -1,5 +1,5 @@
-class Main{
-  public static void main(String[] args){
-    System.out.println("hello world");
-  }
+class First{
+public static void main(String[] args){
+System.out.println("hello worls");
+}
 }
